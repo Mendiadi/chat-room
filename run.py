2f7d6c133cab1035f9004d33d0a513bb030e9938
mendiadi <adim333@gmail.com>
2022-10-25 21:27:01 +0300

    app works fine

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from communication import *
 
 if __name__ == '__main__':
-    server = BindingConnection("127.1.1.1", 5555)
+    server = BindingConnection("127.0.0.1", 5555)
     conn = ConnectionHandler()
     server.start(conn.new_connection)
